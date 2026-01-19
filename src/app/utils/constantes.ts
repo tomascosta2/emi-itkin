63e8a66848c831ec3356adab561ca6a81a796d68
JeronimoCortez <jeronimofcortez3@gmail.com>
2026-01-19 12:26:22 -0300

    Nuevo diseño

---
+++ src/app/utils/constantes.ts
@@ -0,0 +1,220 @@
+export const coachName = "Emiliano Itkin"
+
+export const pixelId = "639700134796854"
+
+export const calendlyBaseUrl = "//calendly.com/emilianoitkincoach/asesoria-1-1?embed_domain=100porcientocalistenia.com&embed_type=Inline"
+
+export const waNumber = "5493446406338";
+
+export const ALT_IMG_GENERIC = "Emiliano Itkin - Fit"
+
+// Calendario: helpers
+export const titleCalender = "Reunión con el equipo de Emi – Plan para recomponer hormonas y músculo";
+export const descriptionCalender = `Reunión 1:1 para alinear objetivos y plan.\nEnlace: MEETING_URL\nTraer dudas y objetivos claros.`;
+export const locationCalender = "Online (se compartirá enlace)";
+
+// VSL
+export const idVsl = "panda-db8732b6-b713-4da2-ba3d-df8726f6f5e3";
+export const srcVsl = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=db8732b6-b713-4da2-ba3d-df8726f6f5e3";
+
+// Thankyou
+export const idThankyou = "panda-55c6aea4-147d-4247-a7c9-e1cca58bfad2";
+export const srcThankyou = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=55c6aea4-147d-4247-a7c9-e1cca58bfad2" ;
+
+
+export const VIDEO_TESTIMONIALS = [
+    {
+        video: "/placeholder.mp4",
+        titulo: "<<LLENAR>>",
+        story: "<<LLENAR>>",
+        nombre: "<<LLENAR>>",
+        dato: "<<LLENAR>>",
+    },
+];
+
+export const TESTIMONIALS = [
+    {
+        img: "/images/testimonios/testimonios/testimonio1.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio2.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio3.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio4.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio5.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio6.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio7.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio8.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio9.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio10.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio11.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio12.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio13.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio14.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio15.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio16.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio17.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio18.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio19.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio20.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio21.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio22.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio23.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio24.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio25.webp",
+    }
+];
+
+// Imagen con texto en testimonios page
+export const MORE_CHANGES_IMG = "/images/testimonios/testimonios/testimonio1.webp";
+
+// Hacemos una constante aparte para modificar puntualmente que imagenes queremos en THANKYOU
+export const TESTIMONIALS_THANKYOU_IMG = [
+    {
+        img: "/images/testimonios/testimonios/testimonio1.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio2.webp",
+    },
+    {
+        img: "/images/testimonios/testimonios/testimonio3.webp",
+    },
+]
+
+// El testimonio que se habilita despues de ver el vsl principal de page
+export const TESTIMONIALS_VIDEO_PAGE = [
+    {
+        video:
+            "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=ba1afc87-a9fc-430d-83aa-143f68dabf5b",
+        titulo: "Una videollamada de 15 minutos me cambió rotundamente",
+        story:
+            "En poco tiempo, ajustando poquitas cosas, enseguida se empezaron a ver los cambios. Encontré una disciplina que se amolda a mi estilo de vida",
+        nombre: "Carlos Andres Paul",
+        dato: "Buenos Aires, Argentina",
+    },
+    {
+        video:
+            "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=2b6ef2c9-fa26-4861-b327-826ca6af4ccb",
+        titulo: "Recomiendo este programa porque me funcionó",
+        story:
+            "Lo que más valoré fue el acompañamiento: nuestras reuniones semanales, la app que utilizamos, donde tenés las dietas, las recetas, la música y el grupo de WhatsApp que te motiva a sumarte.",
+        nombre: "Hugo Reynoso",
+        dato: "Buenos Aires, Argentina",
+    },
+
+    {
+        video:
+            "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=05ae2960-cf86-48a2-b00b-bd7258043a86",
+        titulo: "Tuve los resultados que hace tiempo venía buscando",
+        story:
+            "En el primer chequeo bajé entre 2 y 5 kilos, y siempre tuve un progreso.",
+        nombre: "Dario Gutierrez",
+        dato: "Buenos Aires, Argentina",
+    },
+    {
+        video:
+            "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=db5ef3d1-2167-4292-9dac-78be05e39afe",
+        titulo: "Antes no tenía un seguimiento personalizado",
+        story:
+            "El programa te ofrece hacerlo desde tu casa y una dieta acorde, que no implica un sacrificio.",
+        nombre: "Christian Armesto",
+        dato: "Córdoba, Argentina",
+    }
+];
+
+export const CALENDLY_TITLE_PART1 =
+    "Transformá tu cuerpo en 16 semanas bajando grasa de forma sostenida";
+
+export const CALENDLY_SPAN =
+    "entrenando desde casa con calistenia";
+
+export const CALENDLY_TITLE_PART2 =
+    "Agendá una llamada y veamos cómo hacerlo de manera personalizada.";
+
+export const hotjarID = "<<LLENAR>>"
+
+// Que incluye el metodo landing
+export const METHOD_INCLUDES = [
+    {
+        img: "./images/medical_mobile_app.svg",
+        title: `App Mobile "TestoBoost" Todo en uno`
+    },
+    // {
+    //     img: "./images/hand_with_a_pill.svg",
+    //     title: `Suplementación Natural Diagnosticada por el Dr. Alan Inman`
+    // }, 
+    // {
+    //     img: "./images/frying_pan.svg",
+    //     title: `20 Viandas Saludables Mensuales recetadas por la Lic. Ana buteler`
+    // }, 
+    // {
+    //     img: "./images/gym.svg",
+    //     title: `Sistema Personalizado de Alimentación y Entrenamiento`
+    // },
+    {
+        img: "./images/stethoscope.svg",
+        title: `Diagnóstico y Monitoreo Hormonal`
+    }, 
+    {
+        img: "./images/classroom.svg",
+        title: `Clases de Motivación y Liderazgo`
+    }, 
+    {
+        img: "./images/test_passed.svg",
+        title: `Tacker de Hábitos`
+    }, 
+    {
+        img: "./images/batch_assign.svg",
+        title: `Comunidad Exclusiva`
+    }, 
+    {
+        img: "./images/today.svg",
+        title: `Seguimiento y Acompañamiento Diario`
+    },
+
+]
