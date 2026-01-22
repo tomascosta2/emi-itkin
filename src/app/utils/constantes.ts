54dff4e8ecaf93e90d684600e707e828840ea41d
Tomas Costa <tomascostapp@gmail.com>
2026-01-22 11:36:51 -0300

    vsl test

--- src/app/utils/constantes.ts
+++ src/app/utils/constantes.ts
@@ -14,8 +14,8 @@ export const descriptionCalender = `Reunión 1:1 para alinear objetivos y plan.\
 export const locationCalender = "Online (se compartirá enlace)";
 
 // VSL
-export const idVsl = "panda-db8732b6-b713-4da2-ba3d-df8726f6f5e3";
-export const srcVsl = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com/embed/?v=db8732b6-b713-4da2-ba3d-df8726f6f5e3";
+export const idVsl = "panda-636f6d70-151f-4d4b-8ff9-20657528c779";
+export const srcVsl = "https://player-vz-5c2adb98-6a4.tv.pandavideo.com.br/embed/?v=636f6d70-151f-4d4b-8ff9-20657528c779";
 
 // Thankyou
 export const idThankyou = "panda-55c6aea4-147d-4247-a7c9-e1cca58bfad2";
