87d8fc11a255e796ac6be5edfd791b558a3d4433
Tomas Costa <tomascostapp@gmail.com>
2026-01-25 12:29:45 -0300

    page view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1391736115664543');
+              fbq('init', '1624044918599862');
               fbq('track', 'PageView');
             `,
           }}
@@ -39,7 +39,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1391736115664543&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1624044918599862&ev=PageView&noscript=1"
           />
         </noscript>
 
