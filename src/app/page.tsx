07c2cbd1188c32b2a7ae06620c9ab607fb0c23cb
Tomas Costa <tomascostapp@gmail.com>
2026-02-11 18:01:44 -0300

    titulo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
             </span>
           </h1>
           <p className="text-[var(--primary)] text-center mt-2">
-            No entrenas solo: 5 profesionales trabajan con vos 1 a 1 para llevar tu físico al siguiente nivel.
+            No entrenas solo: 5 profesionales expertos en perdida de peso trabajan con vos 1 a 1 para llevar tu físico al siguiente nivel.
           </p>
           <section className="relative">
             <div className="bg-[#131313] p-1 pt-0 border-1 border-[var(--primary)] overflow-clip rounded-[12px] md:rounded-[16px] border-[var(--primary)] mt-6 max-w-[750px] mx-auto">
