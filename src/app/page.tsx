f3a64cbc4af72f24b3432741e43bca607409ec44
Tomas Costa <tomascostapp@gmail.com>
2026-02-11 18:02:49 -0300

    titulo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
       <header className="bg-linear-0 from-[#0E0E0E] to-[#1C1B1B] max-w-[85%] mt-6 md:mt-12 w-[500px] rounded-full mx-auto border border-[var(--primary)]/30 z-50">
         <div className="cf-container">
           <h3 className="text-center uppercase text-[var(--text-primary)]/80 tracking-widest text-[12px] py-3 leading-[130%]">
-            <span>Programa #1 para hombres sedentarios en LATAM o algo así</span>
+            <span>Programa #1 para hombres sedentarios en LATAM</span>
           </h3>
         </div>
       </header>
